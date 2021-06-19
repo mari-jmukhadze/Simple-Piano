eacfc96af2576c1c66e270501b5b6b2a229a243a
mari-jmukhadze <m_jmukhadze@cu.edu.ge>
2021-06-19 15:30:17 +0400

    input for keys added

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ function play_audio(key){
    var audio = document.getElementById(key).childNodes
    audio[3].play()
 }
+
+function get_value(){
+   var val = document.getElementById('keys').value;
+   var notes = val.split(',')
+   // notes.push
+   for (let i = 0; i < notes.length; i++){
+      play_audio(notes[i])
+   }
+}
